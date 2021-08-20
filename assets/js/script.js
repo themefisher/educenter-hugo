c1c1548b826e86253bf3b06477371623ab36628d
Joep Kokkeler <j.kokkeler@gmail.com>
2021-08-20 16:13:14 +0200

    Added radar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,34 @@
 (function ($) {
-  'use strict';
+  "use strict";
 
-  // Preloader js    
-  $(window).on('load', function () {
-    $('.preloader').fadeOut(100);
+  // Preloader js
+  $(window).on("load", function () {
+    $(".preloader").fadeOut(100);
   });
 
   // Sticky Menu
   $(window).scroll(function () {
-    var height = $('.top-header').innerHeight();
-    if ($('header').offset().top > 10) {
-      $('.top-header').addClass('hide');
-      $('.navigation').addClass('nav-bg');
-      $('.navigation').css('margin-top','-'+height+'px');
+    var height = $(".top-header").innerHeight();
+    if ($("header").offset().top > 10) {
+      $(".top-header").addClass("hide");
+      $(".navigation").addClass("nav-bg");
+      $(".navigation").css("margin-top", "-" + height + "px");
     } else {
-      $('.top-header').removeClass('hide');
-      $('.navigation').removeClass('nav-bg');
-      $('.navigation').css('margin-top','-'+0+'px');
+      $(".top-header").removeClass("hide");
+      $(".navigation").removeClass("nav-bg");
+      $(".navigation").css("margin-top", "-" + 0 + "px");
     }
   });
 
-  
-
   // Background-images
-  $('[data-background]').each(function () {
+  $("[data-background]").each(function () {
     $(this).css({
-      'background-image': 'url(' + $(this).data('background') + ')'
+      "background-image": "url(" + $(this).data("background") + ")",
     });
   });
 
   //Hero Slider
-  $('.hero-slider').slick({
+  $(".hero-slider").slick({
     autoplay: true,
     autoplaySpeed: 7500,
     pauseOnFocus: false,
@@ -38,68 +36,75 @@
     infinite: true,
     arrows: true,
     fade: true,
-    prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'ti-angle-left\'></i></button>',
-    nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'ti-angle-right\'></i></button>',
-    dots: true
+    prevArrow:
+      "<button type='button' class='prevArrow'><i class='ti-angle-left'></i></button>",
+    nextArrow:
+      "<button type='button' class='nextArrow'><i class='ti-angle-right'></i></button>",
+    dots: true,
   });
-  $('.hero-slider').slickAnimation();
+  $(".hero-slider").slickAnimation();
 
   // venobox popup
   $(document).ready(function () {
-    $('.venobox').venobox();
+    $(".venobox").venobox();
   });
 
   // filter
   $(document).ready(function () {
-    var containerEl = document.querySelector('.filtr-container');
+    var containerEl = document.querySelector(".filtr-container");
     var filterizd;
     if (containerEl) {
-      filterizd = $('.filtr-container').filterizr({});
+      filterizd = $(".filtr-container").filterizr({});
     }
     //Active changer
-    $('.filter-controls li').on('click', function () {
-      $('.filter-controls li').removeClass('active');
-      $(this).addClass('active');
+    $(".filter-controls li").on("click", function () {
+      $(".filter-controls li").removeClass("active");
+      $(this).addClass("active");
     });
   });
 
   //  Count Up
   function counter() {
     var oTop;
-    if ($('.count').length !== 0) {
-      oTop = $('.count').offset().top - window.innerHeight;
+    if ($(".count").length !== 0) {
+      oTop = $(".count").offset().top - window.innerHeight;
     }
     if ($(window).scrollTop() > oTop) {
-      $('.count').each(function () {
+      $(".count").each(function () {
         var $this = $(this),
-          countTo = $this.attr('data-count');
+          countTo = $this.attr("data-count");
         $({
-          countNum: $this.text()
-        }).animate({
-          countNum: countTo
-        }, {
-          duration: 1000,
-          easing: 'swing',
-          step: function () {
-            $this.text(Math.floor(this.countNum));
+          countNum: $this.text(),
+        }).animate(
+          {
+            countNum: countTo,
           },
-          complete: function () {
-            $this.text(this.countNum);
+          {
+            duration: 1000,
+            easing: "swing",
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+            },
           }
-        });
+        );
       });
     }
   }
-  $(window).on('scroll', function () {
+  $(window).on("scroll", function () {
     counter();
   });
 
   // Animation
   $(document).ready(function () {
-    $('.has-animation').each(function (index) {
-      $(this).delay($(this).data('delay')).queue(function () {
-        $(this).addClass('animate-in');
-      });
+    $(".has-animation").each(function (index) {
+      $(this)
+        .delay($(this).data("delay"))
+        .queue(function () {
+          $(this).addClass("animate-in");
+        });
     });
   });
 
@@ -169,10 +174,73 @@
     });
   });
 
+  if ($(".rellax").length) {
+    new Rellax(".rellax", {
+      center: true,
+    });
+  }
 
+  if ($("#radar").length) {
+    fetch("/data/radar.csv")
+      .then(function (resp) {
+        return resp.text();
+      })
+      .then(function (csv) {
+        var entries = d3.csvParse(csv, function (row) {
+          return toEntry(row);
+        });
+        draw_radar(entries);
+      });
+  }
 })(jQuery);
 
-var rellax = new Rellax(".rellax", {
-  center: true,
-});
+function draw_radar(my_entries) {
+  techRadarViz({
+    quadrants: quadrants,
+    rings: rings,
+    entries: my_entries,
+  });
+}
 
+function toEntry(row) {
+  return {
+    name: row.name,
+    category: row.quadrant,
+    quadrant: row.quadrant,
+    quadrantIndex: quadrants.indexOf(row.quadrant),
+    ring: rings.findIndex(function (item) {
+      return item.name === row.ring;
+    }),
+    status: row.ring,
+    ringName: row.ring,
+    // link: row.link,
+    // moved: ["down", "none", "up"].indexOf(row.moved) - 1,
+    active: true,
+  };
+}
+
+var rings = [
+  {
+    name: "Hold",
+    color: "#E74C3C",
+  },
+  {
+    name: "Assess",
+    color: "#3498DB",
+  },
+  {
+    name: "Trial",
+    color: "#F1C40F",
+  },
+  {
+    name: "Adopt",
+    color: "#1ABC9C",
+  },
+];
+
+var quadrants = [
+  "Languages and Frameworks",
+  "Platforms",
+  "Tools",
+  "Techniques",
+];
